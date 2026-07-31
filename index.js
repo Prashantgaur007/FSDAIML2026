1a2cb97f88976cea58b242fd1e44b1472d18f492
Prashantgaur007 <prashantgaur581@gmail.com>
2026-07-31 10:26:52 +0530

    #

--- index.js
+++ index.js
@@ -27,16 +27,83 @@
 //     }
 // }
 // loginVerification("0325","0325",login)
-// Callback function
+// Callback functions
 
-function checkNumber(num, clbk) {
-    clbk(num);
-}
-function oddEven(num) {
-    if (num % 2 === 0) {
-        console.log(num + " is Even");
+// function checkNumber(num, clbk) {
+//     clbk(num);
+// }
+// function oddEven(num) {
+//     if (num % 2 === 0) {
+//         console.log(num + " is Even");
+//     } else {
+//         console.log(num + " is Odd");
+//     }
+// }
+// checkNumber(7, oddEven);
+// console.log("1st");
+// setTimeout(() => {
+//     console.log("2nd");
+// }, 1000);
+// for (let i = 0; i < 100; i++) {
+//     console.log("second");
+// }
+// console.log("3rd");
+// console.log("4th");
+
+
+// setTimeout(() => {console.log("1st");
+//     setTimeout(() => {console.log("2nd");
+//         setTimeout(() => {console.log("3rd");
+//             setTimeout(() => {console.log("4th");   
+//                 setTimeout(() => {console.log("5th");
+//                     setTimeout(() => {console.log("6th");
+//                          setTimeout(() => {console.log("7th");
+//                             setTimeout(() => {console.log("8th");
+//                                 setTimeout(() => {console.log("9th");
+//                                     setTimeout(() => {console.log("10th");
+//                                         setTimeout(() => {console.log("11th");
+//                                         }, 1000);
+//                                     }, 1000);
+//                                 }, 1000);
+//                             }, 1000);
+//                         }, 1000);
+//                     }, 1000);
+//                 }, 1000);
+//             }, 1000);
+//         }, 1000);
+//      }, 1000);
+// }, 1000);
+
+// callback hell
+// setTimeout(() => {}, 1000);
+// promises
+
+const myPromise = new Promise((resolve, reject) => {
+    username = "prashant";
+    password = "0325";
+    if (username === "prashant" && password == "0325") {
+        resolve("Login successful");
     } else {
-        console.log(num + " is Odd");
+        reject("Invalid username or password");
+    }})
+    // myPromise.then((msg) => {
+    //     console.log("Success: " + msg);
+    // }).catch((error) => {
+    //     console.log("Error: " + error);
+    //     }).finally(() => {
+    //         console.log("Login process completed"); 
+    // });
+
+    async function handlelogin(){
+        try{
+            await myPromise.then((msg) => {
+                console.log("Success: " + msg);
+            })
+        }catch(error){
+            console.log("Error: "+error);
+        }
+        finally{
+            console.log("Login process completed");
+        }
     }
-}
-checkNumber(7, oddEven);
+    handlelogin();
