de15d935f79ad87ad514c6bbbc1afdaffbe174d1
Prashantgaur007 <prashantgaur581@gmail.com>
2026-09-10 10:27:08 +0530

    image add

--- reactvite/src/Component/ICard.jsx
+++ reactvite/src/Component/ICard.jsx
@@ -1,16 +1,57 @@
 import React from 'react';
+import img from './Images/prashant.jpg';
 
 function ICard() {
-    let a = 10;
-  let name = "John Doe";
     return (
-    <div style={{border: '1px solid black', padding: '20px'}}>
-        <h2 style={{ color: 'blue' }}>ICard</h2>
-      <h2 style={{ color: 'blue' }}>Welcome to Vite</h2>
-      <h2>Value of a: {a}</h2>
-      <h2>Name: {name}</h2>
-    </div>
-    )
+        <div
+            style={{
+                border: '10px solid red',
+                height: '350px',
+                width: '250px',
+                padding: '20px',
+                backgroundColor: 'lightblue',
+                marginLeft: '200px'
+            }}
+        >
+            <img
+                src={img}
+                alt="Prashant Gaur"
+                style={{ width: '100%', height: 'auto' }}
+            />
+
+            <h2 style={{ backgroundColor: 'white', color: 'black' }}>
+                ABES Engineering College
+            </h2>
+
+            <h2 style={{ backgroundColor: 'white', color: 'black' }}>
+                Welcome to Vite
+            </h2>
+
+            <h2 style={{ backgroundColor: 'white', color: 'black' }}>
+                NAME = Prashant Gaur
+            </h2>
+
+            <h2 style={{ backgroundColor: 'white', color: 'black' }}>
+                ROLL NO = 147
+            </h2>
+
+            <h2 style={{ backgroundColor: 'white', color: 'black' }}>
+                BRANCH = AIML
+            </h2>
+
+            <h2 style={{ backgroundColor: 'white', color: 'black' }}>
+                YEAR = 2024-28
+            </h2>
+
+            <div>
+                <img
+                    src={img}
+                    alt="Prashant Gaur"
+                    style={{ width: '100%', height: 'auto' }}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default ICard;
